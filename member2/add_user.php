5739ef075e20ce0a14cbd5ba501a15726685bb1b
page0715 <page0715@gmail.com>
2023-11-13 16:33:01 +0800

    20231113

--- member2/add_user.php
+++ member2/add_user.php
@@ -2,17 +2,10 @@
 $dsn="mysql:host=localhost;charset=utf8;dbname=member";
 $pdo=new PDO($dsn,'root','');
 $acc=htmlspecialchars(trim($_POST['acc']));
-$pw=htmlspecialchars(trim($_POST['pw']));
-$name=htmlspecialchars(trim($_POST['name']));
-$email=htmlspecialchars(trim($_POST['email']));
-$address=htmlspecialchars(trim($_POST['address']));
 
-
-$sql="insert into `users`(`acc`,`pw`,`name`,`email`,`address`)
-        values('{$_POST['acc']}','{$_POST['pw']}','{$_POST['name']}','{$_POST['email']}','{$_POST['address']}');";
+$sql="insert into `users`(`acc`,`pw`,`name`,`email`,`address`) 
+                   values('{$acc}','{$_POST['pw']}','{$_POST['name']}','{$_POST['email']}','{$_POST['address']}')";
 
 $pdo->exec($sql);
-        
+
 header("Location:index.php");
-
-
