9ff42eae784be5d0138cb3a5cf9541636bea81c8
page0715 <page0715@gmail.com>
2023-11-17 16:04:12 +0800

    all() 函式增加第二參數的功能

--- function/db.php
+++ function/db.php
@@ -8,9 +8,38 @@ function all($table=null,$where=''){
     $dsn="mysql:host=localhost;charset=utf8;dbname=school";
     $pdo=new PDO($dsn,'root','');
 
-    if(isset($table)){
+    if(isset($table) && !empty($table)){
+
+        if(is_array($where)){
+            /**
+             * 
+             * [`dept`=>2,`graduate_at`=>12 =] => where `dept`=2 && `graduate_at`='12'
+             * 
+             * $sql="select * from `$table` where `dept`='2' && `graduate_at`='12'"
+             * 
+             */
+            $tsql="select * from `$table`";
+
+            if(!empty($where)){
+                foreach($where as $col => $value){
+                    $tmp[]="`$col`=`$value`";
+                }
+                $sql="select * from `$table` where ".join(" && ",$tmp);
+            
+            }else{
+                $sql="select * from `$table` ";
+            }
+            
+        }else{
+            $sql="select * from `$table` $where;";
+
+        }
+
+
+
+
+
     // $sql="select * from `students`;";
-    $sql="select * from `$table` $where;";
     $rows=$pdo->query($sql)->fetchAll();
     return $rows;
     }else{
